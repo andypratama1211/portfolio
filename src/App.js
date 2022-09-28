1030ef0ab64205a8f427d0bee89ff69c8eb22633
andypratama3_ <andypratama1211@gmail.com>
2022-09-28 12:54:17 +0800

    reinitialized files

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/NavBar.js";
+import { Banner } from "./components/Banner.js";
+import { Skills } from "./components/Skills.js";
+import { Projects } from "./components/Projects.js";
+import { Contact } from "./components/Contact.js";
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects/>
+      <Contact/>
+    </div>
+  );
+}
+
+export default App;
+
